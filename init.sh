64e8c283eb45e1bc0dc5cb5949eecea1e6193d50
altengofso <altengofso@gmail.com>
2021-03-01 00:39:12 +0300

    https://stepik.org/lesson/14826/step/11

--- init.sh
+++ init.sh
@@ -2,5 +2,6 @@ sudo ln -s /home/box/web/etc/nginx.conf  /etc/nginx/sites-enabled/test.conf
 sudo rm -rf /etc/nginx/sites-enabled/default
 sudo /etc/init.d/nginx restart
 sudo ln -s /home/box/web/etc/gunicorn-wsgi.conf   /etc/gunicorn.d/gunicorn-wsgi
+sudo ln -s /home/box/web/etc/gunicorn-django.conf   /etc/gunicorn.d/gunicorn-django
 sudo /etc/init.d/gunicorn restart
 #sudo /etc/init.d/mysql start
