ad738c1793aaf1a2e1829d43bb70db9a9dba25f7
altengofso <altengofso@gmail.com>
2021-03-03 21:40:17 +0300

    2.3.10

--- init.sh
+++ init.sh
@@ -6,4 +6,9 @@ sudo /etc/init.d/nginx restart
 sudo ln -s /home/box/web/etc/gunicorn-wsgi.conf   /etc/gunicorn.d/gunicorn-wsgi
 sudo ln -s /home/box/web/etc/gunicorn-django.conf   /etc/gunicorn.d/gunicorn-django
 sudo /etc/init.d/gunicorn restart
-#sudo /etc/init.d/mysql start
+sudo /etc/init.d/mysql start
+mysql -uroot -e "create database if not exists ask;"
+mysql -uroot -e "grant all privileges on ask.* to 'box'@'localhost';"
+mysql -uroot -e "flush privileges;"
+~/web/ask/manage.py makemigrations
+~/web/ask/manage.py migrate
